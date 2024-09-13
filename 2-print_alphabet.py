e5c3536c8b5584687a6d1b148148ddca4ba0a723
IZERE <blaiseinnocenti@gmail.com>
2024-09-13 01:02:03 -0700

    Practicing python

--- 2-print_alphabet.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_alpha():
-    for i in range(97, 123):
-        print("{}".format(chr(i)), end="")
-
-
-print_alpha()
--- 3-print_alphabt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_alpha():
-    for i in range(97, 123):
-        if chr(i) != 'q' and chr(i) != 'e':
-            print("{}".format(chr(i)), end="")
-
-
-print_alpha()
