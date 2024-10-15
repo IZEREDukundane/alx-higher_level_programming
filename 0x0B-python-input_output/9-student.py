515b8da1237a53995699d73ab37633df0c291034
IZERE <blaiseinnocenti@gmail.com>
2024-10-15 21:01:11 +0000

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Student to JSON module"""
+''' a class Student that defines a student
+'''
 
 
 class Student:
-    """Student class"""
+    '''module class student
+    '''
+
     def __init__(self, first_name, last_name, age):
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of a Student instance"""
-        return self.__dict___
+        '''method to_json
+        '''
+        return self.__dict__
